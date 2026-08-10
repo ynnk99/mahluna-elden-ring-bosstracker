81a999dc8e265c731f77b6dc2b00ae201cb186d1
ynnk99 <yalolp@gmail.com>
2026-08-10 21:19:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ const STREAMER_LOGIN = "mahluna";
 // ── BUILD-PLANNER ──────────────────────────────────────────────────────────
 // Um den aktuell gezeigten Build zu ändern: einfach den Link von
 // tarnished.dev/build-planner hier eintragen.
-const BUILD_PLANNER_URL = "https://www.tarnished.dev/build-planner?str=81&dex=19&int=16&fai=15&arc=8&vig=60&min=10&end=41&somber=10&smith=25&twoHanded=true&selectedClass=Vagabond&LoS=false&scaLvl=12&aRun=false&aFlk=false&aPvp=false&cStr=true&cDex=true&cInt=true&cFai=true&cArc=true&cVig=true&cMin=false&enduranceChange=false&rh1=Large+Club&aRh1=Heavy&sRun=Godrick%27s+Great+Rune&sFlk=Opaline+Bubbletear%2CCrimson+Crystal+Tear&sTal=Erdtree%27s+Favor+%2B1%2CViridian+Amber+Medallion+%2B2%2CCrimson+Amber+Medallion+%2B2%2CTwo-Handed+Sword+Talisman&cht=Armor+of+Solitude&gnt=Gauntlets+of+Solitude&hlm=Helm+of+Solitude&leg=Greaves+of+Solitude";
+const BUILD_PLANNER_URL = "https://www.tarnished.dev/build-planner?str=81&dex=19&int=16&fai=15&arc=8&vig=60&min=10&end=41&somber=10&smith=25&twoHanded=true&selectedClass=Vagabond&LoS=false&scaLvl=12&aRun=false&aFlk=false&aPvp=false&cStr=true&cDex=true&cInt=true&cFai=true&cArc=true&cVig=true&cMin=false&enduranceChange=false&rh1=Large+Club&aRh1=Heavy&sRun=Godrick%27s+Great+Rune&sFlk=Opaline+Bubbletear%2CCrimson+Crystal+Tear&sTal=Erdtree%27s+Favor+%2B1%2CCrimson+Amber+Medallion+%2B2%2CTwo-Handed+Sword+Talisman%2CTwo-Headed+Turtle+Talisman&cht=Armor+of+Solitude&gnt=Gauntlets+of+Solitude&hlm=Helm+of+Solitude&leg=Greaves+of+Solitude";
 
 const FEXTRA_WIKI_BASE = "https://eldenring.wiki.fextralife.com";
 
@@ -193,6 +193,7 @@ const BUILD_ITEM_IMAGES = {
   "Viridian Amber Medallion":     FEXTRA_WIKI_BASE + "/file/Elden-Ring/viridian_amber_medallion_talisman_elden_ring_wiki_guide_200px.png",
   "Pearldrake Talisman":          FEXTRA_WIKI_BASE + "/file/Elden-Ring/pearldrake_talisman_3_talisman_elden_ring_shadow_of_the_erdtree_dlc_wiki_guide_200px.png",
   "Two-Handed Sword Talisman":    FEXTRA_WIKI_BASE + "/file/Elden-Ring/two-handed_sword_talisman_talisman_elden_ring_shadow_of_the_erdtree_dlc_wiki_guide_200px.png",
+  "Two-Headed Turtle Talisman":   FEXTRA_WIKI_BASE + "/file/Elden-Ring/two-headed_turtle_talisman_talisman_elden_ring_shadow_of_the_erdtree_dlc_wiki_guide_200px.png",
 };
 
 const MAIN_BOSSES = new Set([
